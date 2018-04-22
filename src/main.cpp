2e5b39e9ededefc2cc9c46ebccf5c621cbe6326e
stollal <31903712+stollal@users.noreply.github.com>
2018-04-22 12:22:11 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include<cmath>
+class Pyramids{
+	int pyramidsCases(){
+		switch(pyramid){
+			case 1:
+				int baseCase= (pow(x,2)+pow((x-1),2));
+				int i,temp;
+				for(i = x; i<=i+1; i++){
+					int blocks % i = temp;
+				}
+				if(temp== 0)
+					return true;
+				else{
+					return false;
+				}
+			break;
+			case 2:
+				int baseCase2 = (pow(x,2)+pow((x-2),2));
+				int j,temp2;
+				for(j=x; j <= j +1; j++){
+					int blocks % i = temp;
+				}
+				if(temp == 0)
+					return true;
+				else{
+					return false;
+					}
+			break;
+	int calcBlocks(int blocks, int x, int pyramids){
+		for(x=0; x<=blocks; x++){
+			
+	bool checkHighPyramids(){
+
+	}
+	bool checkLowPyramids(){
+
+	}
+	bool checkSolution(){
+
+	}
+	int main(){
+
+	}	
